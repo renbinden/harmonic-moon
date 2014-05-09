eecb97d2bc6343d48f876c4f0a922414457c1e2e
Lucariatias <lucariatias@gmail.com>
2014-05-09 15:16:02 +0100

    Smooth camera movement

--- src/main/java/io/github/lucariatias/harmonicmoon/player/Camera.java
+++ src/main/java/io/github/lucariatias/harmonicmoon/player/Camera.java
@@ -1,23 +1,24 @@
 package io.github.lucariatias.harmonicmoon.player;
 
+import io.github.lucariatias.harmonicmoon.world.Vector;
 import io.github.lucariatias.harmonicmoon.world.WorldLocation;
-import io.github.lucariatias.harmonicmoon.world.WorldPanel;
 
 public class Camera {
 
     private WorldLocation location;
     private Player player;
+    private static final double SPEED = 0.05D;
 
     public Camera(Player player) {
         this.player = player;
-        this.location = player.getCharacter().getWorldInfo().getLocation();
+        this.location = player.getCharacter().getWorldInfo().getLocation().getRelative(new Vector(-320, -240));
     }
 
     public void onTick() {
-        //int targetX = - character.getLocation().getX() + WorldPanel.WIDTH / 2;
-        //int targetY = - character.getLocation().getY() + WorldPanel.HEIGHT / 2;
-        //location = location.getRelative(new Vector((int) Math.round((double) (targetX - location.getX()) * 0.01D), (int) Math.round((double) (targetY - location.getY()) * 0.01D)));
-        location = new WorldLocation(player.getCharacter().getWorldInfo().getLocation().getWorld(), player.getCharacter().getWorldInfo().getLocation().getX() - (WorldPanel.WIDTH / 2), player.getCharacter().getWorldInfo().getLocation().getY() - (WorldPanel.HEIGHT / 2));
+        int targetX = player.getCharacter().getWorldInfo().getLocation().getX() - 320;
+        int targetY = player.getCharacter().getWorldInfo().getLocation().getY() - 240;
+        location = location.getRelative(new Vector((int) Math.round((double) (targetX - location.getX()) * SPEED), (int) Math.round((double) (targetY - location.getY()) * SPEED)));
+        //location = new WorldLocation(player.getCharacter().getWorldInfo().getLocation().getWorld(), player.getCharacter().getWorldInfo().getLocation().getX() - (WorldPanel.WIDTH / 2), player.getCharacter().getWorldInfo().getLocation().getY() - (WorldPanel.HEIGHT / 2));
     }
 
     public WorldLocation getLocation() {
