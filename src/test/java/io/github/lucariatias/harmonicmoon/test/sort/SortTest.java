ca24463e47421cb28660f88608b49a105cfad9c5
Lucariatias <lucariatias@gmail.com>
2014-08-09 14:02:45 +0100

    Convert tests to Lambda Behave

--- src/test/java/io/github/lucariatias/harmonicmoon/test/sort/SortTest.java
+++ src/test/java/io/github/lucariatias/harmonicmoon/test/sort/SortTest.java
@@ -1,40 +1,55 @@
 package io.github.lucariatias.harmonicmoon.test.sort;
 
+import com.insightfullogic.lambdabehave.JunitSuiteRunner;
 import io.github.lucariatias.harmonicmoon.util.sort.Sorter;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SortTest {
+import static com.insightfullogic.lambdabehave.Suite.describe;
 
-    @Test
-    public void testAscendingSort() {
-        List<IntegerSortableWrapper> list = new ArrayList<>();
-        for (int i = 20; i > 0; i--) {
-            list.add(new IntegerSortableWrapper(i));
-        }
-        Sorter<IntegerSortableWrapper> sorter = new Sorter<>(list);
-        List<IntegerSortableWrapper> sorted = sorter.sortAscending();
-        Assert.assertEquals(list.size(), sorted.size());
-        for (int i = 0; i < sorted.size() - 1; i++) {
-            Assert.assertTrue(sorted.get(i).getValue() <= sorted.get(i + 1).getValue());
-        }
-    }
+@RunWith(JunitSuiteRunner.class)
+public class SortTest {{
 
-    @Test
-    public void testDescendingSort() {
-        List<IntegerSortableWrapper> list = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            list.add(new IntegerSortableWrapper(i));
-        }
-        Sorter<IntegerSortableWrapper> sorter = new Sorter<>(list);
-        List<IntegerSortableWrapper> sorted = sorter.sortDescending();
-        Assert.assertEquals(list.size(), sorted.size());
-        for (int i = 0; i < sorted.size() - 1; i++) {
-            Assert.assertTrue(sorted.get(i).getValue() >= sorted.get(i + 1).getValue());
-        }
-    }
+    List<IntegerSortableWrapper> list = new ArrayList<>();
 
-}
+    describe("a descending list", it -> {
+
+        it.isSetupWith(() -> {
+            for (int i = 20; i > 0; i--) {
+                list.add(new IntegerSortableWrapper(i));
+            }
+        });
+
+        it.should("be in ascending order when sorted", (expect) -> {
+            Sorter<IntegerSortableWrapper> sorter = new Sorter<>(list);
+            List<IntegerSortableWrapper> sorted = sorter.sortAscending();
+            expect.that(list.size()).is(sorted.size());
+            for (int i = 0; i < sorted.size() - 1; i++) {
+                expect.that(sorted.get(i).getValue()).isLessThanOrEqualTo(sorted.get(i + 1).getValue());
+            }
+        });
+
+    });
+
+    describe("an ascending list", it -> {
+
+        it.isSetupWith(() -> {
+            for (int i = 0; i < 20; i++) {
+                list.add(new IntegerSortableWrapper(i));
+            }
+        });
+
+        it.should("be in descending order when sorted", (expect) -> {
+            Sorter<IntegerSortableWrapper> sorter = new Sorter<>(list);
+            List<IntegerSortableWrapper> sorted = sorter.sortDescending();
+            expect.that(list.size()).is(sorted.size());
+            for (int i = 0; i < sorted.size() - 1; i++) {
+                expect.that(sorted.get(i).getValue()).isGreaterThanOrEqualTo(sorted.get(i + 1).getValue());
+            }
+        });
+
+    });
+
+}}
